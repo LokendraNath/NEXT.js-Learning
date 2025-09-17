121c8df2075ed3959d40c0966246df2bee7dd62c
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-17 22:55:18 +0530

    settings page setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-900 dark:bg-gray-950 dark:text-gray-100`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
