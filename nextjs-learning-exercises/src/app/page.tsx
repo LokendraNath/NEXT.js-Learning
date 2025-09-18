8067283ed7d2e6d13c0100832d054ce5d676feee
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-18 10:26:01 +0530

    clear prev

--- nextjs-learning-exercises/src/app/page.tsx
+++ nextjs-learning-exercises/src/app/page.tsx
@@ -1,16 +1,4 @@
-import Link from "next/link";
-
 const Home = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus,
-        quam!
-      </p>
-
-      <Link href={"/dashboard"}>Go To Dashboard</Link>
-    </div>
-  );
+  return <div>Home</div>;
 };
 export default Home;
