b7963a6e544c619ea4ae0af7247df6e899761717
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-18 11:39:13 +0530

    create challenge note

--- nextjs-learning-exercises/src/app/page.tsx
+++ nextjs-learning-exercises/src/app/page.tsx
@@ -1,18 +1,4 @@
-"use client";
-import { useRouter } from "next/navigation";
-
 const Home = () => {
-  const router = useRouter();
-  return (
-    <div>
-      <h1>This is use Router Exercise</h1>
-      <button
-        className="btn bg-purple-500"
-        onClick={() => router.push("/page1")}
-      >
-        Go To Page1
-      </button>
-    </div>
-  );
+  return <div>Home</div>;
 };
 export default Home;
