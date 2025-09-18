16e15ec75c16305d1a880ea8e8a367ce423abc2b
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-18 13:42:58 +0530

    ignoring and private route exercise

---
+++ nextjs-learning-exercises/src/app/(auth)/login/page.tsx
@@ -0,0 +1,4 @@
+const Login = () => {
+  return <h1>Login to see the content</h1>;
+};
+export default Login;
--- nextjs-learning-exercises/src/app/page.tsx
+++ nextjs-learning-exercises/src/app/page.tsx
@@ -1,4 +1,9 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <h2>Route Organizng And Ignoring Route in Next.js</h2>
+    </div>
+  );
 };
 export default Home;
