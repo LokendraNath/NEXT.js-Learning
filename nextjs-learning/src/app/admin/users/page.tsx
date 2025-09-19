42356d6eaba71cbde081c8f908735227d121a6ce
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-19 17:07:00 +0530

    different route for layout

---
+++ nextjs-learning/src/app/admin/users/page.tsx
@@ -0,0 +1,8 @@
+const User = () => {
+  return (
+    <div>
+      <h1>User List</h1>
+    </div>
+  );
+};
+export default User;
---
+++ nextjs-learning/src/app/users/layout.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+const PublicLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="min-h-screen flex flex-col text-white bg-[#0A0A0A]">
+      {/* Header */}
+      <header className="py-3 border-b border-gray-100">
+        <nav className="flex items-center justify-between px-10">
+          <h1 className="text-3xl font-bold">Users Layout</h1>
+          <ul className="flex space-x-5">
+            <li>
+              <Link href="/basiclayout">Homepage</Link>
+            </li>
+            <li>
+              <Link href="/basiclayout/about">About</Link>
+            </li>
+            <li>
+              <Link href="/basiclayout/contact">Contact</Link>
+            </li>
+            <li>
+              <Link href="/admin">Login ad Admin</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      {/* Main */}
+      <div className="flex flex-1">
+        {/* Sidebar */}
+        <aside className="w-64 p-5 border-r border-gray-100">
+          <h1>Categories</h1>
+          <ul>
+            <li>
+              <Link href="/basiclayout">- Technology</Link>
+            </li>
+            <li>
+              <Link href="/basiclayout/about">- Design</Link>
+            </li>
+            <li>
+              <Link href="/basiclayout/about">- Business</Link>
+            </li>
+          </ul>
+        </aside>
+        {/* Main Container */}
+        <main className="flex-1 p-5">
+          <div>{children}</div>
+        </main>
+      </div>
+      {/* Footer */}
+      <footer className="text-center border-t border-gray-100">
+        © 2025 Lokendra Nath. All rights reserved.
+      </footer>
+    </div>
+  );
+};
+export default PublicLayout;
