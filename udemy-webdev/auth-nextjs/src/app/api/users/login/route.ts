474155f9570fed5ad2012898e22241d80dd34681
Lokendra Nath <nathdeveloper25@gmail.com>
2025-10-08 23:55:55 +0530

    setup jwt token logic in backend

--- udemy-webdev/auth-nextjs/src/app/api/users/login/route.ts
+++ udemy-webdev/auth-nextjs/src/app/api/users/login/route.ts
@@ -2,6 +2,7 @@ import { connectDB } from "@/config/db";
 import User from "@/modals/usersModal.js";
 import bcrypt from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
 
 connectDB();
 
@@ -21,9 +22,29 @@ export const POST = async (request: NextRequest) => {
 
     // Check If Password is Correct
     const validPassword = await bcrypt.compare(password, user.password);
-    if (validPassword) {
+    if (!validPassword) {
       return NextResponse.json({ error: "Invalid Password" }, { status: 400 });
     }
+
+    // Create Token Data
+    const tokenData = {
+      id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+    };
+
+    // Create Token
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json({
+      message: "Login Successfull",
+      success: true,
+    });
+
+    response.cookies.set("token", token, { httpOnly: true });
+    return response;
   } catch (error) {
     return NextResponse.json(
       { error: (error as Error).message },
