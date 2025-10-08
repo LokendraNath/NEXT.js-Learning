9a7900a4f7fcdb9f1ec31dd6487b9f8bc1e0e416
Lokendra Nath <nathdeveloper25@gmail.com>
2025-10-08 11:50:47 +0530

    create user modal

---
+++ udemy-webdev/auth-nextjs/src/modals/usersModal.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  fullName: { type: String, require: [true, "Please Provide a Fullname"] },
+  email: { type: String, require: [true, "Please Provide a Email"] },
+  password: { type: String, require: [true, "Please Provide a Password"] },
+  isVerified: { type: Boolean, default: false },
+  isAdmin: { type: Boolean, default: false },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
+
+const User = mongoose.model.users || mongoose.model("users", UserSchema);
+
+export default User;
