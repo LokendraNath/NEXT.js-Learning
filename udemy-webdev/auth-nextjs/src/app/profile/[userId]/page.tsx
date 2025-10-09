04d1417b65444726239586e40c3384b77b2fa5d1
Lokendra Nath <nathdeveloper25@gmail.com>
2025-10-09 15:59:27 +0530

    featch user details in profile/userId

--- udemy-webdev/auth-nextjs/src/app/profile/[userId]/page.tsx
+++ udemy-webdev/auth-nextjs/src/app/profile/[userId]/page.tsx
@@ -1,12 +1,25 @@
-const UserProfilePage = ({ params }: { params: { userId: string } }) => {
+import { connectDB } from "@/config/db";
+import User from "@/modals/usersModal";
+
+const UserProfilePage = async ({ params }: { params: { userId: string } }) => {
+  await connectDB();
+
+  const { userId } = params;
+
+  const user = await User.findById(userId).select("-password");
+
+  if (!user)
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        User Not Found
+      </div>
+    );
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold">
-        UserId:{" "}
-        <span className="bg-orange-400 text-black p-3 rounded-xl">
-          {params.userId}
-        </span>
-      </h1>
+      <h1 className="text-3xl font-bold">Full Name: {user.fullName}</h1>
+      <h2 className="text-xl font-bold">Email: {user.email}</h2>
+      <p>UserId: {user._id.toString()}</p>
     </div>
   );
 };
