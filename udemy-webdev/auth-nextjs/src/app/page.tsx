1c77c998584e08af313c4214a8d2eaa1cab8454c
Lokendra Nath <nathdeveloper25@gmail.com>
2025-10-09 11:51:51 +0530

    handle public route by middleware

--- udemy-webdev/auth-nextjs/src/app/page.tsx
+++ udemy-webdev/auth-nextjs/src/app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 const page = () => {
   return (
-    <div className="">
+    <div className="min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-2xl">Next Js Auth</h1>
+      <Link href="/profile">
+        <button className="bg-green-500 p-3 mt-5 rounded-lg font-bold cursor-pointer">
+          Go To Profile
+        </button>
+      </Link>
     </div>
   );
 };
---
+++ udemy-webdev/auth-nextjs/src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const isPublicPath = path === "/login" || path === "/signup";
+  const token = request.cookies.get("token")?.value || "";
+
+  //* Check If User In Public And Token
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  // return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/profile", "/profile/:userId*", "/login", "/signup"],
+};
